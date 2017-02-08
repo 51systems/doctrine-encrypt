340a3617fc3cb356aa6214adb9a63d42081ed452
Dries Vints <dries.vints@gmail.com>
2017-02-08 11:03:09 +0100

    Re-add docblocks

--- src/Encryptors/EncryptorInterface.php
+++ src/Encryptors/EncryptorInterface.php
@@ -11,11 +11,15 @@ interface EncryptorInterface
 {
     /**
      * Must accept data and return encrypted data
+     * @param string $data
+     * @return string
      */
     public function encrypt($data);
 
     /**
      * Must accept data and return decrypted data
+     * @param string $data
+     * @return string
      */
     public function decrypt($data);
 }
